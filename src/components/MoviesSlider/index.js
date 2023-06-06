0128de4f5559c03437dec388e5c3e1633a825b68
AdiChodavarpu <123074909+AdiChodavarpu@users.noreply.github.com>
2023-06-06 18:26:02 +0530

    Update index.js

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -1,6 +1,11 @@
 import Slider from 'react-slick'
 import MovieItem from '../MovieItem'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+
+
 const MoviesSlider = props => {
   const {moviesLists} = props
   const actionMovies = moviesLists.filter(eachitem =>
